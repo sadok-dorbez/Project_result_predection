db31a59db971e3fb1359966162a0a254760e0002
sadok-dorbez <sadokdorbez0@gmail.com>
2023-04-15 16:04:34 +0100

    test predection

--- Projectpredction.py
+++ Projectpredction.py
@@ -7,18 +7,19 @@ from sklearn.metrics import accuracy_score
 df = pd.read_csv('./African projects Dataset.csv')
 
 # count total NaN values across all columns
-print('empty values',df.isna().sum().sum())
+print('empty values', df.isna().sum().sum())
 
 # drop empty values
 df.dropna(inplace=True)
 
 # one-hot method to encode the categorical variables
-df = pd.get_dummies(df, columns=["regionname","countryname","lendinginstr"])
+df = pd.get_dummies(df, columns=["regionname", "countryname", "lendinginstr"])
 
 X = df.drop(['project result'], axis=1)
 y = df['project result']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42)
 
 print('Training set shape:', X_train.shape, y_train.shape)
 print('Testing set shape:', X_test.shape, y_test.shape)
@@ -31,3 +32,22 @@ clf.fit(X_train, y_train)
 y_pred = clf.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 print("Accuracy:", accuracy)
+
+
+# List of columns
+print(X_train.columns)
+
+
+# Predict a new data point
+new_data_point = [[80000000, 0, 0, 0, 0, 0, 1, 0, 0, 0, #1
+                  0, 0, 0, 0, 0, 0, 0, 0, 0, 0,       #2
+                  0, 0, 0, 0, 0, 0, 0, 0, 0, 0,       #3
+                  0, 0, 0, 0, 0, 1, 0, 0, 0, 0,       #4
+                  0, 0, 0, 0, 0, 0, 0, 0, 0, 0,       #5
+                  0, 0, 0, 0, 0, 0, 0, 0, 0, 0,       #6
+                  0, 0, 0, 0, 0, 0, 0, 0, 0, 0,       #7
+                  0, 0, 0, 0, 0, 0, 0, 0, 0, 1,       #8
+                  0, 0, 0, 0, 0, 0, 0, 0, 0, 0,       #9
+                  0, 0, 0]]                           #10
+prediction = clf.predict(new_data_point)
+print("Prediction:", prediction)
